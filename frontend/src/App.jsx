ec2ad7920e8b672275490d8974e2d69e7455ef5c
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-03-27 21:43:22 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center text-blue-700 text-4xl font-bold underline pt-12'>
+      ArtGenie
+    </div>
+  )
+}
+
+export default App
