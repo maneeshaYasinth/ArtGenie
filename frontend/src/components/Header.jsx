edef6169774f56da5cfb320a7f593b86e4dddb54
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-04-24 17:22:21 +0530

    added header paras

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,10 +5,15 @@ const Header = () => {
   return (
     <div>
         <div className='mt-20'>
-            <h1 className="text-4xl font-bold text-center mt-10 font-Pixelify Sans">Art Genie</h1>
-            <div className='flex flex-row items-center justify-center mt-4  '>
-                <p className="text-lg text-center mt-4 border-2 border-gray-700 rounded-2xl px-2">Your AI Art Generator</p>
-                <img src={star_icon} alt="star icon" className='mt-5 px-2'/>
+            <h1 className="text-6xl font-bold text-center mt-10 font-Pixelify Sans">Art Genie</h1>
+            <div className='flex flex-row items-center justify-center mt-8'>
+              <div className=' border-2 border-gray-300 rounded-2xl flex flex-row items-center justify-center bg-cyan-100/50 '>
+                <p className="text-center  px-2 text-gray-500 text-sm">Your AI Art Generator</p>
+                <img src={star_icon} alt="star icon" className='mt-1 mr-2'/>
+                </div>
+            </div>
+            <div>
+              <h1 className='text-5xl text-center mt-8'>Turn Your Text to, Image In Secnds!</h1>
             </div>
         </div>
     </div>
