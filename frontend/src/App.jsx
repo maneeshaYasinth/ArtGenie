addff2626a5a5cb7a286b835ad2fc5df1d69c4ac
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-04-26 12:46:52 +0530

    alignment done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,11 @@ const App = () => {
   return (
     <div className='px-4 sm:px-6 md:px-8 lg:px-16 min-h-screen'>
       <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/results" element={<Results />} />
-      <Route path="/buy-credits" element={<BuyCredits />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/buy-credits" element={<BuyCredits />} />
+      </Routes>
     </div>
   )
 }
